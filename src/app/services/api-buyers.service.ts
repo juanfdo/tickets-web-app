bd51141c2ff139d6bd0b65779c0a02ba24d5a1c2
juan <juan@localhost.localdomain>
2021-03-30 18:52:41 -0500

    fea: Buyer service, added.

--- src/app/services/api-buyers.service.ts
+++ src/app/services/api-buyers.service.ts
@@ -1,9 +1,29 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AppSettings } from "../app.settings";
+import { Observable } from 'rxjs';
+import { Buyer } from '../models/buyer';
 import { Injectable } from '@angular/core';
+
+const httpOption = {
+  headers: new HttpHeaders({
+    'Contend-Type': 'application/json'
+  })
+}
 
 @Injectable({
   providedIn: 'root'
 })
+
 export class ApiBuyersService {
 
-  constructor() { }
+  url: string = AppSettings.API_ENDPOINT + "buyers/";
+  constructor(private _http: HttpClient) { }
+
+  getBuyers(): Observable<Buyer>{
+    return this._http.get<Buyer>(this.url);
+  }
+
+  addBuyer(buyer: Buyer): Observable<Buyer>{
+    return this._http.post<Buyer>(this.url, buyer, httpOption);
+  }
 }
