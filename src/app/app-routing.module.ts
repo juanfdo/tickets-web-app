b300734bf5f180b6e16c694bb921c6e07db71410
juan <juan@localhost.localdomain>
2021-03-30 15:14:06 -0500

    fea: Routes for Ticket registration and selling, and buyer registration components added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,14 +2,18 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { TicketsComponent } from './components/tickets/tickets.component';
 import { BuyersComponent } from './components/buyers/buyers.component';
+import { BuyerRegistrationComponent } from "./components/buyers/buyer-registration/buyer-registration.component";
+import { TicketRegistrationComponent } from "./components/tickets/ticket-registration/ticket-registration.component";
+import { TicketSellingComponent } from "./components/tickets/ticket-selling/ticket-selling.component";
 
 const routes: Routes = [
 
   { path: '', redirectTo: 'login', pathMatch: 'full' },
   { path: 'tickets', component: TicketsComponent, pathMatch: 'full'},
   { path: 'buyers', component: BuyersComponent, pathMatch: 'full'},
-  { path: 'tickets/:id', component: TicketsComponent },
-  { path: 'buyers/:id', component: BuyersComponent },
+  { path: 'tickets/registration', component: TicketRegistrationComponent },
+  { path: 'buyers/registration', component: BuyerRegistrationComponent },
+  { path: 'tickets/:id/selling', component: TicketSellingComponent },
 
 //  { path: 'ficha/:casoId', component: FichaComponent, canActivate: [AuthGuard], data: {roles: [1,2,3,4]} },
 //  { path: 'derivacion/:fichaId', component: DerivacionComponent, canActivate: [AuthGuard], data: {roles: [2,3,4]} },
