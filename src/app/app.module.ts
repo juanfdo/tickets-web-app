11f340a971b645e29d7c59c0870ee831c197747f
juan <juan@localhost.localdomain>
2021-03-30 05:57:20 -0500

    fea: App modules, updated. Adds new components to app modules.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,16 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { BuyersComponent } from './components/buyers/buyers.component';
+import { TicketsComponent } from './components/tickets/tickets.component';
+import { MenuComponent } from './components/menu/menu.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BuyersComponent,
+    TicketsComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule
