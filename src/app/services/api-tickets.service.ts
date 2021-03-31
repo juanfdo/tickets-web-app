24d2723eed3fc2068308d392ad6828ed6448bf92
juan <juan@localhost.localdomain>
2021-03-30 20:00:40 -0500

    fea: Ticket service, added methods. Added getTickets method. Added addTicket method.

--- src/app/services/api-tickets.service.ts
+++ src/app/services/api-tickets.service.ts
@@ -1,9 +1,27 @@
+import { HttpClient, HttpHeaders } from '@angular/common/http';
+import { AppSettings } from "../app.settings";
+import { Observable } from 'rxjs';
+import { Ticket } from '../models/ticket';
 import { Injectable } from '@angular/core';
 
+const httpOption = {
+  headers: new HttpHeaders({
+    'Contend-Type': 'application/json'
+  })
+}
 @Injectable({
   providedIn: 'root'
 })
 export class ApiTicketsService {
 
-  constructor() { }
+  url: string = AppSettings.API_ENDPOINT + "tickets/";
+  constructor(private _http: HttpClient) { }
+
+  getTickets(): Observable<Ticket>{
+    return this._http.get<Ticket>(this.url);
+  }
+
+  addTicket(ticket: Ticket): Observable<Ticket>{
+    return this._http.post<Ticket>(this.url, ticket, httpOption);
+  }
 }
