763bdc417b9c0376f929fca2aed0d7ff473f21b0
juan <juan@localhost.localdomain>
2021-03-31 02:35:04 -0500

    fea: AppSettings for DataTables options.

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -1,3 +1,27 @@
 export class AppSettings {
   public static API_ENDPOINT='http://localhost:3000/api/';
+
+  public static DATA_TABLES_OPTIONS= {
+    pagingType: 'full_numbers',
+    processing: true,
+    responsive: true,
+    language: {
+      processing: "Procesando...",
+      search: "Buscar:",
+      lengthMenu: "Mostrar _MENU_ elementos",
+      info: "Mostrando desde _START_ al _END_ de _TOTAL_ elementos",
+      infoEmpty: "Mostrando ningún elemento.",
+      infoFiltered: "(filtrado _MAX_ elementos total)",
+      infoPostFix: "",
+      loadingRecords: "Cargando registros...",
+      zeroRecords: "No se encontraron registros",
+      emptyTable: "No hay datos disponibles en la tabla",
+        paginate: {
+          first: "Primero",
+          previous: "Anterior",
+          next: "Siguiente",
+          last: "Último"
+        }
+     }
+  }
 }
