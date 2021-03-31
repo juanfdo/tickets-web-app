daa754c9f14ca9a9f0e68b461b1786edebf87be6
juan <juan@localhost.localdomain>
2021-03-31 02:41:12 -0500

    fea: Tickets component, implemented.

--- src/app/components/tickets/tickets.component.ts
+++ src/app/components/tickets/tickets.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { DataTablesModule } from 'angular-datatables';
+import { Ticket } from 'src/app/models/ticket';
+import { AppSettings } from "src/app/app.settings";
+import { Subject } from 'rxjs';
+import { ApiTicketsService } from 'src/app/services/api-tickets.service';
 
 @Component({
   selector: 'app-tickets',
@@ -6,10 +11,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./tickets.component.css']
 })
 export class TicketsComponent implements OnInit {
+  
+  dtOptions: DataTables.Settings = {};
+  ticketsList: Ticket[] = [];
+  dtTrigger: Subject<any> = new Subject<any>();
+  ready: boolean = false;
 
-  constructor() { }
+  constructor(private apiTicketsService: ApiTicketsService) { }
 
   ngOnInit(): void {
+    this.dtOptions = AppSettings.DATA_TABLES_OPTIONS;
+    this.getTickets();
   }
 
+  getTickets(){
+    this.apiTicketsService.getTickets().subscribe(
+      response => {
+        this.ticketsList = response;
+        this.dtTrigger.next();
+        this.ready = true;
+      },
+      error => {
+
+      }
+    )
+  }
 }
