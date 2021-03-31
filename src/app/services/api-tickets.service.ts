b5f0e2020ee37572650191f5b11485b6969090cf
juan <juan@localhost.localdomain>
2021-03-31 00:32:45 -0500

    fea: Ticket service, methods implementation. Added getTicket method. Added sellTicket method. Added getAvailable method. Fixed getTickets interface.

--- src/app/services/api-tickets.service.ts
+++ src/app/services/api-tickets.service.ts
@@ -3,6 +3,8 @@ import { AppSettings } from "../app.settings";
 import { Observable } from 'rxjs';
 import { Ticket } from '../models/ticket';
 import { Injectable } from '@angular/core';
+import { TicketXBuyer } from '../models/ticket-x-uyer';
+import { AvailableTickets } from '../models/available-tickets';
 
 const httpOption = {
   headers: new HttpHeaders({
@@ -17,11 +19,33 @@ export class ApiTicketsService {
   url: string = AppSettings.API_ENDPOINT + "tickets/";
   constructor(private _http: HttpClient) { }
 
-  getTickets(): Observable<Ticket>{
-    return this._http.get<Ticket>(this.url);
+  getTickets(): Observable<Ticket[]>{
+    return this._http.get<Ticket[]>(this.url);
   }
 
   addTicket(ticket: Ticket): Observable<Ticket>{
     return this._http.post<Ticket>(this.url, ticket, httpOption);
+  }
+
+  getTicket(id: BigInt): Observable<Ticket>{
+    return this._http
+      .get<Ticket>(
+        this.url + id,
+        httpOption);
+  }
+
+  sellTicket(ticketXBuyer: TicketXBuyer): Observable<TicketXBuyer>{
+    return this._http
+      .post<TicketXBuyer>(
+        this.url + ticketXBuyer.txb_ticket_id + '/sells',
+        ticketXBuyer,
+        httpOption);
+  }
+
+  getAvailable(id: BigInt): Observable<AvailableTickets>{
+    return this._http
+      .get<AvailableTickets>(
+        this.url + id + '/available',
+        httpOption);
   }
 }
