308b29d96210207171c01556201a90d898b68f21
juan <juan@localhost.localdomain>
2021-03-31 03:31:58 -0500

    fea: Buyers component, implemented.

--- src/app/components/buyers/buyers.component.ts
+++ src/app/components/buyers/buyers.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { DataTablesModule } from 'angular-datatables';
+import { Buyer } from 'src/app/models/buyer';
+import { AppSettings } from "src/app/app.settings";
+import { Subject } from 'rxjs';
+import { ApiBuyersService } from 'src/app/services/api-buyers.service';
+import Swal from 'sweetalert2';
 
 @Component({
   selector: 'app-buyers',
@@ -6,10 +12,34 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./buyers.component.css']
 })
 export class BuyersComponent implements OnInit {
+  
+  dtOptions: DataTables.Settings = {};
+  buyersList: Buyer[] = [];
+  dtTrigger: Subject<any> = new Subject<any>();
+  ready: boolean = false;
 
-  constructor() { }
+  constructor(private apiBuyersService: ApiBuyersService) { }
 
   ngOnInit(): void {
+    this.dtOptions = AppSettings.DATA_TABLES_OPTIONS;
+    this.getBuyers();
   }
 
+  getBuyers(){
+    this.apiBuyersService.getBuyers().subscribe(
+      response => {
+        this.buyersList = response;
+        this.dtTrigger.next();
+        this.ready = true;
+      },
+      error => {
+        Swal.fire({
+          icon: 'error',
+          title: 'Lo sentimos.',
+          text: 'No fue posible obtener la lista de compradores, intentelo más tarde.',
+          footer: 'Agradecemos su comprensión.'
+        });
+      }
+    )
+  }
 }
