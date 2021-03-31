cdc2cb7e7f6a6bdcd47a95590ddc06c01383ff0a
juan <juan@localhost.localdomain>
2021-03-30 21:08:34 -0500

    fea: Ticket registration component, implemented.

--- src/app/components/tickets/ticket-registration/ticket-registration.component.ts
+++ src/app/components/tickets/ticket-registration/ticket-registration.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { ApiTicketsService } from 'src/app/services/api-tickets.service';
+import Swal from 'sweetalert2';
+
+import { Ticket } from "../../../models/ticket";
 
 @Component({
   selector: 'app-ticket-registration',
@@ -7,9 +13,87 @@ import { Component, OnInit } from '@angular/core';
 })
 export class TicketRegistrationComponent implements OnInit {
 
-  constructor() { }
+  ticket: Ticket = {
+    id: null,
+    tic_event_name: null,
+    tic_quantity: null,
+    tic_sold: null,
+  };
+
+  submitted = false;
+  ticketForm: FormGroup;
+
+  onSubmit() {
+    
+    this.submitted = true;
+    if (this.ticketForm.invalid) {
+      Swal.fire({
+        icon: 'error',
+        title: 'Lo sentimos.',
+        text: 'Las Boletas no se pudieron guardar, por favor verifique los mensajes bajo los campos diligenciados para corregir la información.',
+        footer: 'Agradecemos su comprensión.'
+      });
+      return;
+    }
+
+    Swal.fire({
+      title: '¿Está seguro de que desea guardar los cambios?',
+      showCancelButton: true,
+      confirmButtonText: `Guardar`,
+      cancelButtonText: 'Cancelar'
+    }).then((result) => {
+      if (result.isConfirmed) {
+        this.apiTicketsService.addTicket(this.ticket).subscribe(
+          response => {
+            debugger;
+            Swal.fire('¡Felicidades!', 'Las boletas fueron guardadas satisfactoriamente.', 'success');
+            this.ticketForm.reset();
+            this.submitted = false;
+            this.router.navigate(['tickets']);
+          },
+
+          error => {
+            if(error.status == 422){
+              if(error["error"]?.errors?.tic_event_name[0] === "The tic event name has already been taken."){
+                this.ticketForm.controls.tic_event_name.setErrors({unique: true});
+                Swal.fire({
+                  icon: 'error',
+                  title: 'Lo sentimos.',
+                  text: 'No fue posible guardar las boletass en la base de datos, el nombre del evento ya existe en la base de datos.',
+                  footer: 'Agradecemos su comprensión.'
+                });
+                return;
+              }
+            }
+
+            Swal.fire({
+              icon: 'error',
+              title: 'Lo sentimos.',
+              text: 'No fue posible guardar las boletass en la base de datos, por favor verifique la información ingresada.',
+              footer: 'Agradecemos su comprensión.'
+            });
+          }
+        )
+
+      }
+    })
+  }
+
+  cancel() {}
+
+  constructor(
+    private apiTicketsService: ApiTicketsService,
+    private formBuilder: FormBuilder,
+    private router: Router
+    ){}
 
   ngOnInit(): void {
+    this.ticketForm = this.formBuilder.group({
+      'tic_quantity': ['', [Validators.required, Validators.pattern("^[0-9]*$")]],
+      'tic_event_name': ['', Validators.required]
+    });
   }
+
+  get f() { return this.ticketForm.controls; }
 
 }
